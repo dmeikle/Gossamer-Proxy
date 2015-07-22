743839b69a912c46c736bf8d29001b4fdb4431aa
Nathan DeVuono <nathan.devuono@gmail.com>
2015-07-22 08:18:18 -0700

    Gruntfile tweaks, widgetAdmin mostly created

---
+++ src/components/widgetAdmin/ng/js/widgetAdminDirectives.js
@@ -0,0 +1,24 @@
+module.directive('widgetAdminList', function($compile, templateSrv){
+  var template = templateSrv;
+  return {
+    restrict: 'E',
+    templateUrl: template.widgetAdminList,
+    link: function(scope, element) {
+      scope.addNewWidgetRow = function(){
+        // element.getElementsByTagName('td')[0].before($compile('<widget-admin-list-row></widget-admin-list-row>')(scope));
+        element.after($compile('<widget-admin-list-row></widget-admin-list-row>')(scope));
+      };
+    }
+  };
+});
+
+module.directive('widgetAdminListRow', function(templateSrv){
+  var template = templateSrv;
+  return {
+    restrict:'E',
+    templateUrl: template.widgetAdminListRow,
+    scope: {
+      newWidget: '=ngModel'
+    }
+  };
+});
