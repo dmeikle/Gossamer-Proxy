835b8a4a537de251662e8f2781d8934b56c21e9b
Dave Meikle <david@quantumunit.com>
2015-07-30 15:21:53 -0700

    added custom call for page widgets list

---
+++ src/framework/core/components/widgets/controllers/WidgetPageWidgetsController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ *  This file is part of the Quantum Unit Solutions development package.
+ * 
+ *  (c) Quantum Unit Solutions <http://github.com/dmeikle/>
+ * 
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+namespace core\components\widgets\controllers;
+
+use core\AbstractController;
+
+/**
+ * WidgetPagesController
+ *
+ * @author Dave Meikle
+ */
+class WidgetPageWidgetsController extends AbstractController {
+    
+    public function listallByPage($widgetId) {
+        $result = $this->model->listallByPage(intval($widgetId));
+        
+        $serializer = new \core\components\widgets\serialization\WidgetPageWidgetsSerializer();
+        
+        $this->render($serializer->formatResults($result));
+    }
+}
