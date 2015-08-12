16361e65184f17cacda575750753d2a224f1e2d3
Dave Meikle <david@quantumunit.com>
2015-08-12 13:59:02 -0700

    added locale file for widgets

---
+++ src/framework/core/components/widgets/locales/en_US/Widgets.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+  'WIDGET_PAGE_NAME' => 'Widget Page Name',
+    'WIDGET_PAGE_YML_KEY' => 'YML Key',
+    'WIDGET_PAGE_HTMLKEY' => 'HTML Key',
+    'WIDGET_PAGE_DESCRIPTION' => 'Description',
+    'WIDGET_PAGE_WIDGETS' => 'Widget List',
+    'WIDGET_NAME' => 'Name',
+    'WIDGET_DESCRIPTION' => 'Description',
+    'WIDGET_SECTION' => 'Section',
+    'WIDGET_PAGE_CONFIRM' => 'Confirm',
+    'WIDGET_PAGE_CANCEL' => 'Cancel',
+    'WIDGET_PAGE_NEW' => 'Create New Page',
+    'WIDGET_PAGE_IS_SYSTEM' => 'System Widget',
+    'WIDGET_NEW' => 'Create New Widget'
+    
+);
