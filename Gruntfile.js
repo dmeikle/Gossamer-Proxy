1f2ba6d52cf17f460dff2ede473ffa482caeead8
Nathan DeVuono <nathan.devuono@gmail.com>
2015-08-21 10:56:12 -0700

    Swapped out grunt-contrib-sass for grunt-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -198,7 +198,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
 
   // grunt.registerTask('watch', ['watch']);
 
