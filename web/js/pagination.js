ce0ff374d5514db85e703098495dcba66115d618
Nathan DeVuono <nathan.devuono@gmail.com>
2015-08-21 15:17:12 -0700

    Putting the web directory back. Whoops

---
+++ web/js/pagination.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+
+    
+    $('.pagination').click(function(e) {
+        e.stopPropagation();
+        var url = $(this).data('url');
+       
+        window.location = url + '/' + $(this).data('offset') + '/' + $(this).data('limit');
+    });
+
+    $('#resultsPerPage').change(function() {
+        var num = $('#resultsPerPage').val();
+        var url = $(this).data('url');
+        window.location.href = url + '/0/' + num;
+    });
+});
