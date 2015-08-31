be5ab60c3b0a2976d50656f06d9b8533db5fa023
Dave Meikle <david@quantumunit.com>
2015-08-31 16:38:07 -0700

    added new validation message

--- src/components/staff/locales/en_US/validation.php
+++ src/components/staff/locales/en_US/validation.php
@@ -5,5 +5,6 @@ return array (
     'VALIDATION_PASSWORD_REQUIRED' => 'Password is required',
     'VALIDATION_PASSWORD_MISMATCH' => 'Passwords do not match',
     'VALIDATION_PASSWORD_COMPLEXITY' => 'Password is not complex enough',
-    'VALIDATION_USERNAME_EXISTS' => 'Username already exists'
+    'VALIDATION_USERNAME_EXISTS' => 'Username already exists',
+    'VALIDATION_PASSWORD_IN_HISTORY' => 'Password already exists in user history'
 );
