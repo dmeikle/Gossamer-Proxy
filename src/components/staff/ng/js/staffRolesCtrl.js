152c04837edf93db72befd4113024e660be6c4d8
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-04 14:53:03 -0700

    Save almost working, just need structure for object

---
+++ src/components/staff/ng/js/staffRolesCtrl.js
@@ -0,0 +1,23 @@
+module.controller('staffRolesCtrl', function($scope, $location, staffSrv) {
+  // Stuff to run on controller load
+  $scope.staffRoles = {};
+  $scope.staffRoles.loading = true;
+  getStaffRoles();
+
+  function getStaffRoles() {
+    var object = {};
+    object.id = $location.absUrl().substring($location.absUrl().lastIndexOf('/') + 1, $location.absUrl().length);
+    staffSrv.getStaffRoles(object).then(function() {
+      $scope.staffRoles = staffSrv.staffRoles;
+      $scope.staffRoles.loading = false;
+    });
+  }
+
+  $scope.submitRoles = function(object) {
+    var formToken = document.getElementById('FORM_SECURITY_TOKEN').value;
+    object.id = $location.absUrl().substring($location.absUrl().lastIndexOf('/') + 1, $location.absUrl().length);
+    staffSrv.saveRoles(object).then(function() {
+      getStaffRoles();
+    });
+  };
+});
