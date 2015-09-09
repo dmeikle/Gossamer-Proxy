ad5238a9f65984d9ef5e8a03bf8b010f1979a03c
Dave Meikle <david@quantumunit.com>
2015-09-09 13:46:57 -0700

    added loader for form builders to render.yml

---
+++ src/framework/core/components/render/eventlisteners/FormBuilderListener.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ *  This file is part of the Quantum Unit Solutions development package.
+ * 
+ *  (c) Quantum Unit Solutions <http://github.com/dmeikle/>
+ * 
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace core\components\render\eventlisteners;
+
+use Gossamer\CMS\Forms\FormBuilder;
+use core\components\render\serialization\DependencySerializer;
+
+/**
+ * FormBuilderListener
+ *
+ * @author Dave Meikle
+ */
+class FormBuilderListener extends \core\eventlisteners\AbstractCachableListener{
+    
+    public function on_filerender_start($params) {
+        
+        if(!array_key_exists('formBuilder', $this->listenerConfig)) {
+            throw new Exception('formBuilder key missing from listener config', 508, null);
+        } elseif(!array_key_exists('model', $this->listenerConfig)) {
+            throw new Exception('model key missing from listener config', 508, null);
+        } 
+        
+        $modelClass = $this->listenerConfig['model'];
+        $builderClass = $this->listenerConfig['formBuilder'];
+        
+        $model = new $modelClass($this->httpRequest, $this->httpResponse, $this->logger);
+        $builder = new FormBuilder($this->logger, $model);
+        
+        $formBuilder = new $builderClass();        
+
+        $this->httpResponse->setAttribute('form', $formBuilder->buildForm($builder, array(), $this->getDependencies(), array()));
+    }
+    
+    protected function getDependencies() {
+        if(!array_key_exists('dependencies', $this->listenerConfig)) {
+            return array();
+        }
+        
+        $retval = array();
+        
+        foreach($this->listenerConfig['dependencies'] as $dependency) {
+            $retval[$dependency['key']] = $this->formatDependency($dependency);
+        }
+      
+        return $retval;
+    }
+    
+    protected function formatDependency(array $dependency) {
+        $serializer = new DependencySerializer();
+      
+        return $serializer->formatSelectionBox($dependency['value'], $dependency['text'], $this->httpRequest->getAttribute($dependency['dependency']));
+    }
+}
