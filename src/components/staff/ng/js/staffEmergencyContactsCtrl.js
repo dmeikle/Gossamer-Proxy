d1bf87c392d98f6c5724b776c18aef0039082eb1
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-10 14:25:28 -0700

    Mostly working, waiting on backend for save

---
+++ src/components/staff/ng/js/staffEmergencyContactsCtrl.js
@@ -0,0 +1,53 @@
+module.controller('staffEmergencyContactsCtrl', function($scope, $location, $modal, staffEmergencyContactsSrv, templateSrv) {
+  $scope.loading = true;
+  getStaffEmergencyInfo();
+
+  function getStaffEmergencyInfo() {
+    var object = {};
+    object.id = $location.absUrl().substring($location.absUrl().lastIndexOf('/') + 1, $location.absUrl().length);
+
+    staffEmergencyContactsSrv.getStaffEmergencyInfo(object).then(function() {
+      $scope.staffEmergencyContacts = staffEmergencyContactsSrv.staffEmergencyContacts;
+      $scope.loading = false;
+    });
+  }
+
+  $scope.openEditEmergencyContactModal = function(contact) {
+    var template = templateSrv.editEmergencyContactModal;
+    var modalInstance = $modal.open({
+      templateUrl: template,
+      controller: 'staffEmergencyContactModalCtrl',
+      size: 'lg',
+      resolve: {
+        contact: function() {
+          return contact;
+        }
+      }
+    });
+
+    modalInstance.result.then(function(contact) {
+      var formToken = document.getElementById('FORM_SECURITY_TOKEN').value;
+      contact.staffId = $location.absUrl().substring($location.absUrl().lastIndexOf('/') + 1, $location.absUrl().length);
+      $scope.loading = true;
+      staffEmergencyContactsSrv.save(contact, formToken).then(function() {
+        getStaffEmergencyInfo();
+      });
+    });
+  };
+});
+
+module.controller('staffEmergencyContactModalCtrl', function($scope, $location, $modalInstance, contact) {
+  if (contact) {
+    $scope.contact = contact;
+  } else {
+    $scope.contact = {};
+  }
+
+  $scope.confirm = function() {
+    $modalInstance.close($scope.contact);
+  };
+
+  $scope.close = function() {
+    $modalInstance.dismiss('close');
+  };
+});
