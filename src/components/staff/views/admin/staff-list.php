afdd944dba73406f865b0756c2a5bdd7c3864d01
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-14 08:25:37 -0700

    Ready to start staff/new

--- src/components/staff/views/admin/staff-list.php
+++ src/components/staff/views/admin/staff-list.php
@@ -1,23 +1,17 @@
 <div class="widget" ng-controller="staffListCtrl">
   <div class="widget-content" ng-class="{'panel-open': selectedStaff}">
     <h1 class="pull-left">Staff List</h1>
-    <a href="staff/edit/0" class="pull-right"><?php echo $this->getString('STAFF_NEW');?></a>
+    <a href="staff/edit/0" class="pull-right btn btn-primary"><?php echo $this->getString('STAFF_NEW');?></a>
     <div class="clearfix"></div>
-    <div class="pull-right">
+    <div class="searchbar form-inline">
       <button class="btn-link" ng-click="openStaffAdvancedSearchModal()">
         <?php echo $this->getString('STAFF_ADVANCED_SEARCH') ?>
       </button>
-      <input type="text" list="autocomplete-list" ng-model="basicSearch.val[0]" ng-model-options="{debounce:500}">
+      <input class="form-control" type="text" list="autocomplete-list" ng-model="basicSearch.val[0]">
       <datalist id="autocomplete-list">
         <option ng-if="!autocomplete.length > 0" value=""><?php echo $this->getString('STAFF_LOADING'); ?></option>
         <option ng-repeat="value in autocomplete" value="{{value.firstname}} {{value.lastname}}"></option>
       </datalist>
-      <select name="basicSearchCol" id="basic-search-col" ng-model="basicSearch.col[0]"
-        ng-init="basicSearch.col[0] = 'name'">
-        <option value="name" ng-selected="true"><?php echo $this->getString('STAFF_NAME');?></option>
-        <option value="ext"><?php echo $this->getString('STAFF_EXT');?></option>
-        <option value="phone"><?php echo $this->getString('STAFF_PHONE');?></option>
-      </select>
       <button class="primary" ng-click="search(basicSearch)">
         <?php echo $this->getString('STAFF_SEARCH') ?>
       </button>
@@ -27,7 +21,7 @@
             <tr>
                 <th><?php echo $this->getString('STAFF_NAME'); ?></th>
                 <th><?php echo $this->getString('STAFF_TITLE'); ?></th>
-                <th><?php echo $this->getString('STAFF_EXT'); ?></th>
+                <th><?php echo $this->getString('STAFF_EXTENSION'); ?></th>
                 <th><?php echo $this->getString('STAFF_MOBILE'); ?></th>
                 <th><?php echo $this->getString('STAFF_STATUS'); ?></th>
                 <th><?php echo $this->getString('STAFF_LAST_LOGIN'); ?></th>
@@ -46,16 +40,17 @@
             <td></td>
             <td></td>
           </tr>
-          <tr ng-if="!loading" ng-repeat="staff in staffList" ng-click="selectRow(staff)" ng-class="{'selected': staff.clicked, 'inactive': staff.status=='inactive'}">
-              <td><a href="mailto:{{staff.email}}">{{staff.lastname}}, {{staff.firstname}}</a></td>
-              <td>{{staff.title}}</td>
-              <td>{{staff.telephone}}</td>
-              <td>{{staff.mobile}}</td>
-              <td>{{staff.status}}</td>
-              <td>{{staff.lastLogin}}</td>
+          <tr ng-if="!loading" ng-repeat="staff in staffList" ng-class="{'selected': staff.clicked, 'inactive': staff.status=='inactive'}">
+              <td ng-click="selectRow(staff)"><a href="mailto:{{staff.email}}">{{staff.lastname}}, {{staff.firstname}}</a></td>
+              <td ng-click="selectRow(staff)">{{staff.title}}</td>
+              <td ng-click="selectRow(staff)">{{staff.telephone}}</td>
+              <td ng-click="selectRow(staff)">{{staff.mobile}}</td>
+              <td ng-click="selectRow(staff)">{{staff.status}}</td>
+              <td ng-click="selectRow(staff)">{{staff.lastLogin}}</td>
               <td class="row-controls">
                 <div class="dropdown">
-                  <button class="btn btn-default dropdown-toggle glyphicon glyphicon-cog" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"></button>
+                  <button class="btn btn-default dropdown-toggle glyphicon glyphicon-cog" type="button"
+                    id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"></button>
                   <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
                     <li><a ng-click="openStaffScheduleModal(staff)">Schedule</a></li>
                     <li><a href="edit/{{staff.id}}">Edit</a></li>
@@ -74,7 +69,7 @@
   </div>
 
   <div class="widget-side-panel">
-    <h1>{{selectedStaff.firstname}} {{selectedStaff.lastname}}</h1>
+    <h1><a href="edit/{{selectedStaff.id}}">{{selectedStaff.firstname}} {{selectedStaff.lastname}}</a></h1>
     <h4><?php echo $this->getString('STAFF_TELEPHONE')?></h3>
     <p>{{selectedStaff.telephone}}</p>
     <h4><?php echo $this->getString('STAFF_MOBILE')?></h3>
