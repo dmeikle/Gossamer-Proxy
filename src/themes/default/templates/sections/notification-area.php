f220a4fde682233085f4cddbefe979c5fd958ce9
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-15 10:35:22 -0700

    removing pr()

--- src/themes/default/templates/sections/notification-area.php
+++ src/themes/default/templates/sections/notification-area.php
@@ -12,5 +12,3 @@
 <!-- TAKE THESE OUT -->
 <button ng-click="addNewAlert({domNodeId: 'notificationArea', message: 'Uhoh! A warning!', type:'warning'})">New Alert</button>
 <button ng-click="addNewAlert({domNodeId: 'notificationArea', message: 'Good thing we got rid of that warning!', type:'success'})">New Alert 2</button>
-
-<?php echo pr($this->data) ?>
