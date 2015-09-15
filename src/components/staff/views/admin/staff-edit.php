229c21df88cc25bece81e5ed86bd66bd62416511
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-15 08:37:06 -0700

    Datepicker tweaks

--- src/components/staff/views/admin/staff-edit.php
+++ src/components/staff/views/admin/staff-edit.php
@@ -212,13 +212,29 @@
             </div>
             <div class="form-group">
               <label for="staff-hireDate"><?php echo $this->getString('STAFF_HIREDATE'); ?></label>
-              <input class="form-control" type="date" name="hireDate"
-                id="staff-hireDate" ng-model="staff.hireDate" ng-model-options="{timezone: '+0000'}">
+              <div class="input-group">
+                <input type="date" name="dob" id="staff-hireDate" ng-model="staff.hireDate" ng-model-options="{timezone: '+0000'}"
+                  class="form-control" datepicker-popup is-open="isOpen.hireDate"
+                  datepicker-options="dateOptions" ng-required="true" close-text="<?php echo $this->getString('STAFF_CLOSE');?>" />
+                <span class="input-group-btn" data-datepickername="hireDate">
+                  <button type="button" class="btn-default" data-datepickername="hireDate" ng-click="openDatepicker($event)">
+                    <i class="glyphicon glyphicon-calendar"></i>
+                  </button>
+                </span>
+              </div>
             </div>
             <div class="form-group">
               <label for="staff-departureDate"><?php echo $this->getString('STAFF_DEPARTUREDATE'); ?></label>
-              <input class="form-control" type="date" name="departureDate"
-                id="staff-departureDate" ng-model="staff.departureDate" ng-model-options="{timezone: '+0000'}">
+              <div class="input-group">
+                <input type="date" name="departureDate" id="staff-departureDate" ng-model="staff.departureDate" ng-model-options="{timezone: '+0000'}"
+                  class="form-control" datepicker-popup is-open="isOpen.departureDate"
+                  datepicker-options="dateOptions" ng-required="true" close-text="<?php echo $this->getString('STAFF_CLOSE');?>" />
+                <span class="input-group-btn" data-datepickername="departureDate">
+                  <button type="button" class="btn-default" data-datepickername="departureDate" ng-click="openDatepicker($event)">
+                    <i class="glyphicon glyphicon-calendar"></i>
+                  </button>
+                </span>
+              </div>
             </div>
             <div class="form-group">
               <label for="staff-extension"><?php echo $this->getString('STAFF_EXTENSION'); ?></label>
@@ -339,7 +355,7 @@
           </div>
         </div>
         <div class="cardright" ng-if="!loading">
-          <img class="pull-right" ng-src="/images/staff/{{staff.imageName}}">
+          <img class="staff-picture pull-right" ng-src="/images/staff/{{staff.imageName}}">
         </div>
         <div class="clearfix"></div>
       </div>
