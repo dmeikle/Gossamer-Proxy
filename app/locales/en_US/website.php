ca8521afdcc3b6496a7d7b9fb4c92ed9d02c6eeb
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-17 10:03:27 -0700

    some langstrings in

--- app/locales/en_US/website.php
+++ app/locales/en_US/website.php
@@ -1,6 +1,10 @@
 <?php
 
-  return array(
-    'WEBSITE_SERVICES' => 'Services',
-    'WEBSITE_LOGIN' => 'Services',
-  );
+return array(
+  'WEBSITE_SERVICES' => 'Services',
+  'WEBSITE_LOGIN' => 'Login',
+  'WEBSITE_BLOGS_LIST' => 'Blog',
+  'WEBSITE_CONTACT' => 'Contact',
+  'WEBSITE_FAQS' => 'FAQ',
+  'WEBSITE_ABOUT' => 'About Us',
+);
