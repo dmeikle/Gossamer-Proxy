194a210727d36533461a3adc16763c3f329968fd
Nathan DeVuono <nathan.devuono@gmail.com>
2015-09-18 14:21:39 -0700

    admin-header is now aware whether something belongs in usermenu

--- src/themes/default/templates/sections/admin-header.php
+++ src/themes/default/templates/sections/admin-header.php
@@ -16,48 +16,50 @@
       <ul class="navbar-left">
         <?php
         foreach ($NAVIGATION as $key => $item) {
-          //first check for top parent nav items
-          if (array_key_exists('active', $item) && $item['active'] == false) {
-            //it's not active (do not display) but during development let's see everything
-            //we can remove this line when we are ready to hide them from the real users
-            ?>
-            <li title="disabled on this release"><?php echo $this->getString($item['text_key']); ?></li>
-            <?php
-            continue;
-          }
+          if (!array_key_exists('section', $item)) {
+            //first check for top parent nav items
+            if (array_key_exists('active', $item) && $item['active'] == false) {
+              //it's not active (do not display) but during development let's see everything
+              //we can remove this line when we are ready to hide them from the real users
+              ?>
+              <li title="disabled on this release"><?php echo $this->getString($item['text_key']); ?></li>
+              <?php
+              continue;
+            }
 
-          //before drawing the link, determine if we need a caret or not
-          $hasChildren = array_key_exists('children', $item);
-          $caret = $hasChildren ? ' <span class="caret"></span>' : '';
+            //before drawing the link, determine if we need a caret or not
+            $hasChildren = array_key_exists('children', $item);
+            $caret = $hasChildren ? ' <span class="caret"></span>' : '';
 
-          //now, let's display the link for the top parent item nav
-          if (!$hasChildren) { ?>
-            <li><a href="<?php echo $item['pattern']; ?>"><?php echo $this->getString($item['text_key']) . $caret; ?></a></li>
+            //now, let's display the link for the top parent item nav
+            if (!$hasChildren) { ?>
+              <li><a href="<?php echo $item['pattern']; ?>"><?php echo $this->getString($item['text_key']) . $caret; ?></a></li>
 
-          <?php } else { ?>
-            <li class='dropdown'>
-              <a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo $item['pattern']; ?>">
-                <?php echo $this->getString($item['text_key']) . $caret; ?>
-              </a>
-              <ul class="dropdown-menu">
-                  <?php
-                    foreach ($item['children'] as $childKey => $childItem) {
-                      if (array_key_exists('active', $childItem) && $childItem['active'] == false) {
-                          //it's not active (do not display) but during development let's see everything
-                          //we can remove this line when we are ready to hide them from the real users
-                          ?>
-                          <li title="disabled on this release"><?php echo $this->getString($childItem['text_key']); ?></li>
-                          <?php
-                          continue;
-                      } ?>
-                      <li>
-                        <a href="<?php echo $childItem['pattern']; ?>">
-                          <?php echo $this->getString($childItem['text_key']); ?>
-                        </a>
-                      </li>
-                    <?php } ?>
-              </ul><!--- close child ul -->
-            </li>
+            <?php } else { ?>
+              <li class='dropdown'>
+                <a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo $item['pattern']; ?>">
+                  <?php echo $this->getString($item['text_key']) . $caret; ?>
+                </a>
+                <ul class="dropdown-menu">
+                    <?php
+                      foreach ($item['children'] as $childKey => $childItem) {
+                        if (array_key_exists('active', $childItem) && $childItem['active'] == false) {
+                            //it's not active (do not display) but during development let's see everything
+                            //we can remove this line when we are ready to hide them from the real users
+                            ?>
+                            <li title="disabled on this release"><?php echo $this->getString($childItem['text_key']); ?></li>
+                            <?php
+                            continue;
+                        } ?>
+                        <li>
+                          <a href="<?php echo $childItem['pattern']; ?>">
+                            <?php echo $this->getString($childItem['text_key']); ?>
+                          </a>
+                        </li>
+                      <?php } ?>
+                </ul><!--- close child ul -->
+              </li>
+            <?php } ?>
           <?php } ?>
         <?php } ?>
       </ul>
@@ -71,10 +73,54 @@
               <img class="userimage" src="http://placehold.it/25x25" alt=""> User Name <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Your Tickets</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Logout</a></li>
+            <?php
+            foreach ($NAVIGATION as $key => $item) {
+              if (array_key_exists('section', $item) && $item['section'] == 'usermenu') {
+                //first check for top parent nav items
+                if (array_key_exists('active', $item) && $item['active'] == false) {
+                  //it's not active (do not display) but during development let's see everything
+                  //we can remove this line when we are ready to hide them from the real users
+                  ?>
+                  <li title="disabled on this release"><?php echo $this->getString($item['text_key']); ?></li>
+                  <?php
+                  continue;
+                }
+
+                //before drawing the link, determine if we need a caret or not
+                $hasChildren = array_key_exists('children', $item);
+                $caret = $hasChildren ? ' <span class="caret"></span>' : '';
+
+                //now, let's display the link for the top parent item nav
+                if (!$hasChildren) { ?>
+                  <li><a href="<?php echo $item['pattern']; ?>"><?php echo $this->getString($item['text_key']) . $caret; ?></a></li>
+
+                <?php } else { ?>
+                  <li class='dropdown'>
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo $item['pattern']; ?>">
+                      <?php echo $this->getString($item['text_key']) . $caret; ?>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <?php
+                          foreach ($item['children'] as $childKey => $childItem) {
+                            if (array_key_exists('active', $childItem) && $childItem['active'] == false) {
+                                //it's not active (do not display) but during development let's see everything
+                                //we can remove this line when we are ready to hide them from the real users
+                                ?>
+                                <li title="disabled on this release"><?php echo $this->getString($childItem['text_key']); ?></li>
+                                <?php
+                                continue;
+                            } ?>
+                            <li>
+                              <a href="<?php echo $childItem['pattern']; ?>">
+                                <?php echo $this->getString($childItem['text_key']); ?>
+                              </a>
+                            </li>
+                          <?php } ?>
+                    </ul><!--- close child ul -->
+                  </li>
+                <?php } ?>
+              <?php } ?>
+            <?php } ?>
           </ul>
         </li>
       </ul>
