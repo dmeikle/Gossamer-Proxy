1a9e6000d34962fa92e4eff24a541f2cdd815251
dcdrawk <dcdrawk@gmail.com>
2015-09-23 13:47:47 -0700

    adding untracked files

---
+++ src/extensions/angular/ng/js/phoenixToastsSrv.js
@@ -0,0 +1,19 @@
+module.service('toastsSrv', function() {
+
+  var self = this;
+
+  this.alerts = {};
+
+  this.newAlert = function(alert) { //Expects {domNodeId: <value>, message: <value>, type: <error, info, warning, success>}
+    if (!self.alerts[alert.domNodeId]) {
+      self.alerts[alert.domNodeId] = [];
+    }
+    if (alert.hasOwnProperty('domNodeId') && alert.hasOwnProperty('message')&& alert.hasOwnProperty('type')) {
+      self.alerts[alert.domNodeId].push(alert);
+    }
+  };
+
+  this.dismissAlert = function(alert) {
+    self.alerts[alert.domNodeId].splice(self.alerts[alert.domNodeId].indexOf(alert), 1);
+  };
+});
