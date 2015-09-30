c16fcad4d4d1c4cb5fe61d9793ab0444b6dca8a4
Dave Meikle <david@quantumunit.com>
2015-09-30 08:21:04 -0700

    added edit page for project

--- src/components/projects/locales/en_US/Project.php
+++ src/components/projects/locales/en_US/Project.php
@@ -1,6 +1,8 @@
 <?php
 
 return array(
+    'PROJECTS_ADD' => 'Add',
+    'PROJECTS_EDIT' => 'Edit',
     'PROJECTS_NEW' => 'New Building',
     'PROJECTS_ADVANCED_SEARCH' => 'Advanced Search',
     'PROJECTS_LIST' => 'List Projects',
@@ -17,6 +19,19 @@ return array(
     'PROJECTS_CONFIRM' => 'Confirm',
     'PROJECTS_CANCEL' => 'Cancel',
     'PROJECTS_NORESULTS' => 'No results found',
-    'PROJECTS_BUILDINGAGE' => 'Building Age'
+    'PROJECTS_BUILDINGAGE' => 'Building Age',
+    'PROJECTS_BUILDING_INFO' => "Building Information",
+    'PROJECTS_BUILDING_DETAILS' => "Building Details",
+    'PROJECTS_STRATANAME' => "Strata Name",
+    'PROJECTS_NUMFLOORS' => "Number of Floors",
+    'PROJECTS_NUMUNITS' => "Number of Units",
+    'PROJECTS_PROPERTYTYPE' => "Property Type",
+    'PROJECTS_TELEPHONE' => "Telephone",
+    'PROJECTS_MANAGMENT' => "Management",
+    'PROJECTS_MAINIMAGE' => "Main Image",
+    'PROJECTS_GOOGLEMAPLINK' => "Google Map Link",
+    'PROJECTS_CLAIMS_HISTORY' => "Claims History",
+    'PROJECTS_CONTACT_INFO' => "Contact Information"
+
     
 );
