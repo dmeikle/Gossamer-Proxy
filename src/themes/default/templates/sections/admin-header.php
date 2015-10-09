8481fff31c4395bbdd16f048db7eecba4f419680
dcdrawk <dcdrawk@gmail.com>
2015-10-09 10:18:25 -0700

    changed tabs to use navigation-display.yml to draw the ng-click params

--- src/themes/default/templates/sections/admin-header.php
+++ src/themes/default/templates/sections/admin-header.php
@@ -12,32 +12,7 @@
                 <img class="default-logo" src="/images/logo.png" alt="Logo">
             </a>
         </div>
-        <!--
-                <div id="bs-example-navbar-collapse" class="collapse navbar-collapse">
-                    <ul class="navbar-left">
-                        <li><a href="/admin/dashboard" class="active">Home</a></li>
-                        <li class="dropdown">
-                            <a href="" class="dropdown-toggle" data-toggle="dropdown">Claims <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="/admin/claims">Claims List</a></li>
-                                <li><a href="#">Your Claims</a></li>
-                                <li><a href="#">New Claim</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="">Accounting</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Directory <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="">Clients</a></li>
-                                <li><a href="">Customers</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="/admin/staff">Staff</a>
-                        </li>
-                    </ul>
-        -->
-
+       
 
         <div id="bs-example-navbar-collapse" class="collapse navbar-collapse">
             <ul class="navbar-left">
@@ -59,8 +34,11 @@
 
                     //now, let's display the link for the top parent item nav
                     if (!$hasChildren) {
-                        if(array_key_exists('ng-click', $item)) {?>
-                            <li><a ng-click="<?php echo $item['ng-click']; ?>"><?php echo $this->getString($item['text_key']) . $caret; ?></a></li>
+                        if(array_key_exists('ng-click', $item)) {
+                            $tmp = str_replace('text_key', $this->getString($item['text_key']),  $item['ng-click']);
+                            $ngLink = str_replace('templateUrl', $this->getString($item['templateUrl']),  $tmp);
+                            ?>
+                            <li><a ng-click="<?php echo $ngLink; ?>"><?php echo $this->getString($item['text_key']) . $caret; ?></a></li>
                         <?php 
                         } else {
                         ?>
@@ -81,8 +59,11 @@
                                         <?php
                                         continue;
                                     }
-                                    if(array_key_exists('ng-click', $item)) {?>
-                                        <li><a ng-click="<?php echo $childItem['ng-click']; ?>"><?php echo $this->getString($childItem['text_key']) . $caret; ?></a></li>
+                                    if(array_key_exists('ng-click', $childItem)) {
+                                         $tmp = str_replace('text_key', $this->getString($childItem['text_key']),  $childItem['ng-click']);
+                                        $ngLink = str_replace('templateUrl', $this->getString($childItem['templateUrl']),  $tmp);
+                                        ?>
+                                        <li><a ng-click="<?php echo $ngLink; ?>"><?php echo $this->getString($childItem['text_key']) . $caret; ?></a></li>
                                     <?php 
                                     } else {
                                     ?>
@@ -129,7 +110,7 @@
         <tab ng-repeat="tab in tabs track by $index" active="tab.active" disable="tab.disabled">
             <tab-heading>{{tab.title}}<span ng-click="closeTab($index)" class='close-tab glyphicon glyphicon-remove'></span></tab-heading>
             <div ng-if="tab.loading" class="tab-loader"><span class="spinner-loader"></span></div>
-            <div ng-include="tab.template" onload="hideSpinner(tab)"></div>
+            <div ng-include="tab.templateUrl" onload="hideSpinner(tab)"></div>
         </tab>
     </tabset>
 </div>
