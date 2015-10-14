ddd4ffae3616e82f675bd65f31d8b45646a153e5
dcdrawk <dcdrawk@gmail.com>
2015-10-14 14:05:34 -0700

    working on search

--- src/components/inventory/controllers/SuppliesUsedController.php
+++ src/components/inventory/controllers/SuppliesUsedController.php
@@ -2,29 +2,26 @@
 
 /*
  *  This file is part of the Quantum Unit Solutions development package.
- * 
+ *
  *  (c) Quantum Unit Solutions <http://github.com/dmeikle/>
- * 
+ *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
  */
 
-namespace components\accounting\controllers;
+namespace components\inventory\controllers;
 
 use core\AbstractController;
+use core\system\Router;
 
+class SuppliesUsedController extends AbstractController {
 
-/**
- * Description of PropertiesController
- *
- * @author Dave Meikle
- */
-class SuppliesUsedController extends AbstractController{
-   
     public function search() {
         
         $result = $this->model->search($this->httpRequest->getQueryParameters());
         
         $this->render($result);
     }
+
+
 }
