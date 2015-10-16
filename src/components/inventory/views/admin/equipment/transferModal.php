4c31fa8f416fcd5ba3f3849cf643d5db47f4236b
Nathan DeVuono <nathan.devuono@gmail.com>
2015-10-16 08:55:53 -0700

    Weird serializer error

--- src/components/inventory/views/admin/equipment/transferModal.php
+++ src/components/inventory/views/admin/equipment/transferModal.php
@@ -2,7 +2,7 @@
   <h1><?php echo $this->getString('TRANSFER') ?></h1>
 </div>
 <div class="modal-content">
-  
+  <wizard data-module="inventory" data-filename="inventoryTransferWizardPages"></wizard>
 </div>
 <div class="modal-footer">
 
