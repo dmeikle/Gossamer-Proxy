11a6a304d82575a288d41cc44ab177a0c91389f4
Dave Meikle <david@quantumunit.com>
2015-11-04 14:11:29 -0800

    tweeked datepicker in rootapp

--- src/extensions/angular/ng/js/rootAppDatepickerConfig.js
+++ src/extensions/angular/ng/js/rootAppDatepickerConfig.js
@@ -1,5 +1,3 @@
-var module = angular.module('rootApp', ['ui.bootstrap']);
-
 module.config(function (datepickerConfig, datepickerPopupConfig) {
     datepickerConfig.showWeeks = false;
     datepickerPopupConfig.showButtonBar = false;
