0aa25f9c960bc1e069582b56384a9dd3a91fb0c5
Dave Meikle <david@quantumunit.com>
2015-11-13 08:10:49 -0800

    fixedlayout on edit page

--- src/components/subcontractors/views/admin/edit.php
+++ src/components/subcontractors/views/admin/edit.php
@@ -1,84 +1,84 @@
-<div class="row-controls" style="float: right">
-    <div class="dropdown">
-        <button class="btn btn-default dropdown-toggle glyphicon glyphicon-cog" type="button"
-                id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"></button>
-        <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
-            <li><a href="#" ng-click="openEditModal(claim)"><?php echo $this->getString('SUBCONTRACTORS_EDIT') ?></a></li>
-        </ul>
+<div class="widget col-md-5" ng-controller="subcontractorsEditCtrl">
+    <div class="row-controls" style="float: right">
+        <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle glyphicon glyphicon-cog" type="button"
+                    id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"></button>
+            <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
+                <li><a href="#" ng-click="openEditModal(claim)"><?php echo $this->getString('SUBCONTRACTORS_EDIT') ?></a></li>
+            </ul>
+        </div>
     </div>
-</div>
-<h1 ng-if="!subcontractor"><?php echo $this->getString('SUBCONTRACTORS_CREATE'); ?></h1>
-<h1 class="pull-left" ng-if="subcontractor"><?php echo $this->getString('SUBCONTRACTORS_EDIT') ?> {{subcontractor.companyName}}</h1>
-<div class="clearfix"></div>
-<div class="cards">
-    <div class="leftcard">
-        <form role="form" class="form-standard" method="post" id="subcontractor-form" ng-controller="subcontractorsEditCtrl">
-            <?php echo $form['id']; ?>
-            <table class="table">
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_NAME'); ?>:</td>
-                    <td><?php echo $form['companyName']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_TYPE'); ?>:</td>
-                    <td>
-                        <?php echo $form['SubcontractorTypes_id']; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_ADDRESS'); ?>:</td>
-                    <td><?php echo $form['address1']; ?>
-                        <?php echo $form['address2']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_CITY'); ?>:</td>
-                    <td><?php echo $form['city']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_PROVINCE'); ?>:</td>
-                    <td><?php echo $form['Provinces_id']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_POSTAL_CODE'); ?>:</td>
-                    <td><?php echo $form['postalCode']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_TELEPHONE'); ?>:</td>
-                    <td><?php echo $form['telephone']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_FAX'); ?>:</td>
-                    <td><?php echo $form['fax']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_URL'); ?>:</td>
-                    <td><?php echo $form['url']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_EMAIL'); ?>:</td>
-                    <td><?php echo $form['email']; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $this->getString('SUBCONTRACTORS_RATING'); ?>:</td>
-                    <td><?php echo $form['rating']; ?></td>
-                </tr>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>
-                        <button class="primary" ng-click="save(subcontractor)" ng-disabled="!subcontractor">
-                            <?php echo $this->getString('SUBCONTRACTORS_CONFIRM'); ?>
-                        </button>
-                        <button ng-click="cancel()"><?php echo $this->getString('SUBCONTRACTORS_CANCEL'); ?></button>
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </div>
-</div>
-<div class="cards">
-    <div class="md-col-1"></div>
+    <h1 ng-if="!subcontractor"><?php echo $this->getString('SUBCONTRACTORS_CREATE'); ?></h1>
+    <h1 class="pull-left" ng-if="subcontractor"><?php echo $this->getString('SUBCONTRACTORS_EDIT') ?> {{subcontractor.companyName}}</h1>
+    <div class="clearfix"></div>
 
-    <table class="table table-over table-striped" ng-controller="subcontractorsClaimsListCtrl">
+
+    <form role="form" class="form-standard" method="post" id="subcontractor-form" >
+        <?php echo $form['id']; ?>
+        <table class="table">
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_NAME'); ?>:</td>
+                <td><?php echo $form['companyName']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_TYPE'); ?>:</td>
+                <td>
+                    <?php echo $form['SubcontractorTypes_id']; ?>
+                </td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_ADDRESS'); ?>:</td>
+                <td><?php echo $form['address1']; ?>
+                    <?php echo $form['address2']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_CITY'); ?>:</td>
+                <td><?php echo $form['city']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_PROVINCE'); ?>:</td>
+                <td><?php echo $form['Provinces_id']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_POSTAL_CODE'); ?>:</td>
+                <td><?php echo $form['postalCode']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_TELEPHONE'); ?>:</td>
+                <td><?php echo $form['telephone']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_FAX'); ?>:</td>
+                <td><?php echo $form['fax']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_URL'); ?>:</td>
+                <td><?php echo $form['url']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_EMAIL'); ?>:</td>
+                <td><?php echo $form['email']; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $this->getString('SUBCONTRACTORS_RATING'); ?>:</td>
+                <td><?php echo $form['rating']; ?></td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <button class="primary" ng-click="save(subcontractor)" ng-disabled="!subcontractor">
+                        <?php echo $this->getString('SUBCONTRACTORS_CONFIRM'); ?>
+                    </button>
+                    <button ng-click="cancel()"><?php echo $this->getString('SUBCONTRACTORS_CANCEL'); ?></button>
+                </td>
+            </tr>
+        </table>
+    </form>
+
+</div>
+<div class="widget col-md-offset-1 col-md-5" ng-controller="subcontractorsClaimsListCtrl">
+
+    <table class="table table-over table-striped">
         <thead>
             <tr>
                 <th class="col-md-2">Job Number</th>
@@ -101,4 +101,5 @@
                 class="pagination" boundary-links="true" rotate="false">
     </pagination>
 
+    <div class="clearfix"></div>
 </div>
