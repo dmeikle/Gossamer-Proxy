18b3339beafa5a23314b65375d8ab5508c547e3b
Nathan DeVuono <nathan.devuono@gmail.com>
2015-11-25 15:35:03 -0800

    saves are working, can add locations, spinner while generating emerg #

--- src/components/claims/views/admin/claims/list.php
+++ src/components/claims/views/admin/claims/list.php
@@ -72,8 +72,16 @@
                                 id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                             </button>
                             <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
-                                <li><a href="/admin/claims/edit/{{claim.jobNumber}}"><?php echo $this->getString('CLAIMS_EDIT'); ?></a></li>
+                                <li>
+                                    <a href="/admin/claims/edit/{{claim.unassignedJobNumber}}" ng-if="!claim.jobNumber">
+                                        <?php echo $this->getString('CLAIMS_EDIT'); ?>
+                                    </a>
+                                    <a href="/admin/claims/edit/{{claim.jobNumber}}" ng-if="claim.jobNumber">
+                                        <?php echo $this->getString('CLAIMS_EDIT'); ?>
+                                    </a>
+                                </li>
                                 <li ng-if="!claim.jobNumber"><a href="" ng-click="assignPM(claim)"><?php echo $this->getString('CLAIMS_ASSIGN_PM'); ?></a></li>
+                                <li><a href="" ng-click="remove(claim)"><?php echo $this->getString('CLAIMS_REMOVE') ?></a></li>
                             </ul>
                         </div>
                     </td>
