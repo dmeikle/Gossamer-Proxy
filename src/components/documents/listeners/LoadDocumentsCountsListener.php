a0e6e284455c63b3e3df208979f22a99311340ce
Nathan DeVuono <nathan.devuono@gmail.com>
2015-12-08 13:59:51 -0800

    Passing off to Dave for backend work on document upload

---
+++ src/components/documents/listeners/LoadDocumentsCountsListener.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ *  This file is part of the Quantum Unit Solutions development package.
+ *
+ *  (c) Quantum Unit Solutions <http://github.com/dmeikle/>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace components\documents\listeners;
+
+use components\claims\models\ClaimDocumentModel;
+
+/**
+ * LoadDocumentsCountsListener
+ *
+ * @author Dave Meikle
+ */
+class LoadDocumentsCountsListener extends \core\eventlisteners\AbstractListener {
+
+    public function on_request_start($params) {
+
+        $path = __UPLOADED_FILES_PATH . 'documents' . DIRECTORY_SEPARATOR . $this->getClaimId() . DIRECTORY_SEPARATOR;
+        $locations = $this->getClaimLocations();
+
+
+        $folderList = array('list' => array());
+        $totalFiles = 0;
+        foreach ($locations as $claimLocationId => $unit) {
+            if (file_exists($path . $claimLocationId)) {
+                $folderList['list'][$claimLocationId]['count'] = iterator_count(new \DirectoryIterator($path . $claimLocationId)) - 2; //remove . and ..
+            } else {
+                $folderList['list'][$claimLocationId]['count'] = 0; //nothing uploaded yet
+            }
+        }
+
+        $this->httpResponse->setAttribute('folderList', $folderList);
+    }
+
+    private function getClaimLocations() {
+
+        $datasource = $this->getDatasource('components\\claims\\models\\ClaimDocumentModel');
+        $model = new ClaimDocumentModel($this->httpRequest, $this->httpResponse, $this->logger);
+        $params = array('Claims_id' => $this->getClaimId());
+
+        return $this->formatUnitNumbers($datasource->query('get', $model, 'getunitnumbers', $params));
+    }
+
+    private function getClaimId() {
+        $params = $this->httpRequest->getParameters('Claims_id');
+
+        return intval($params[0]);
+    }
+
+    private function formatUnitNumbers(array $units) {
+        $retval = array();
+        foreach ($units['unitNumbers'] as $unit) {
+            $retval[$unit['id']] = $unit['unitNumber'];
+        }
+        return $retval;
+    }
+
+}
---
+++ src/components/documents/listeners/UploadDocumentsListener.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ *  This file is part of the Quantum Unit Solutions development package.
+ *
+ *  (c) Quantum Unit Solutions <http://github.com/dmeikle/>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace components\documents\listeners;
+
+use core\eventlisteners\AbstractListener;
+
+/**
+ * Description of UploadDocumentsListener
+ *
+ * @author Dave Meikle
+ */
+class UploadDocumentsListener extends AbstractListener {
+
+    public function on_request_start($params = array()) {
+
+        $filenames = array();
+        $requestParams = $this->httpRequest->getParameters();
+        $locationId = $requestParams[0] . '/' . $requestParams[1];
+        $modelName = $this->listenerConfig['class'];
+        $model = new $modelName($this->httpRequest, $this->httpResponse, $this->logger);
+
+        $filepath =  $model->getUploadPath() . DIRECTORY_SEPARATOR . $locationId;   //__SITE_PATH . "/../locationImages/$locationId";
+        $this->prepareDirectory($filepath);
+        
+        foreach ($_FILES['file']['name'] as $index => $file) {
+           if (move_uploaded_file($_FILES['file']['tmp_name'][$index], $filepath . DIRECTORY_SEPARATOR . $_FILES['file']['name'][$index])) {
+                $filenames[] = $filepath . DIRECTORY_SEPARATOR . $_FILES['file']['name'][$index];
+            } 
+        }
+        
+
+        $this->httpRequest->setAttribute('uploadedFiles', $filenames);
+    }
+
+    private function prepareDirectory($filepath) {
+
+        if (!file_exists($filepath)) {
+            if (!mkdir($filepath, 0777, true)) {
+                die("failed to create " . $filepath);
+            }
+        }
+    }
+
+}
