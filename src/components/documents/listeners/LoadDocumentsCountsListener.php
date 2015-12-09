e34ddc19a6d63b7a8fe0c145d3b14111da5bfeb5
Dave Meikle <david@quantumunit.com>
2015-12-09 09:01:09 -0800

    added document count to modal load

--- src/components/documents/listeners/LoadDocumentsCountsListener.php
+++ src/components/documents/listeners/LoadDocumentsCountsListener.php
@@ -23,43 +23,37 @@ class LoadDocumentsCountsListener extends \core\eventlisteners\AbstractListener 
     public function on_request_start($params) {
 
         $path = __UPLOADED_FILES_PATH . 'documents' . DIRECTORY_SEPARATOR . $this->getClaimId() . DIRECTORY_SEPARATOR;
-        $locations = $this->getClaimLocations();
+        $count = $this->getDocumentCount();
 
 
-        $folderList = array('list' => array());
-        $totalFiles = 0;
-        foreach ($locations as $claimLocationId => $unit) {
-            if (file_exists($path . $claimLocationId)) {
-                $folderList['list'][$claimLocationId]['count'] = iterator_count(new \DirectoryIterator($path . $claimLocationId)) - 2; //remove . and ..
-            } else {
-                $folderList['list'][$claimLocationId]['count'] = 0; //nothing uploaded yet
-            }
-        }
+//        $folderList = array('list' => array());
+//        foreach ($locations as $claimLocationId => $unit) {
+//            if (file_exists($path . $claimLocationId)) {
+//                $folderList['list'][$claimLocationId]['count'] = iterator_count(new \DirectoryIterator($path . $claimLocationId)) - 2; //remove . and ..
+//            } else {
+//                $folderList['list'][$claimLocationId]['count'] = 0; //nothing uploaded yet
+//            }
+//        }
 
-        $this->httpResponse->setAttribute('folderList', $folderList);
+        $this->httpResponse->setAttribute('documentCount', $count);
     }
 
-    private function getClaimLocations() {
+    private function getDocumentCount() {
 
         $datasource = $this->getDatasource('components\\claims\\models\\ClaimDocumentModel');
         $model = new ClaimDocumentModel($this->httpRequest, $this->httpResponse, $this->logger);
         $params = array('Claims_id' => $this->getClaimId());
 
-        return $this->formatUnitNumbers($datasource->query('get', $model, 'getunitnumbers', $params));
+        $count = $datasource->query('get', $model, 'list', $params);
+        if (array_key_exists('ClaimDocumentsCount', $count)) {
+            return $count['ClaimDocumentsCount'][0]['rowCount'];
+        }
     }
 
     private function getClaimId() {
         $params = $this->httpRequest->getParameters('Claims_id');
 
         return intval($params[0]);
-    }
-
-    private function formatUnitNumbers(array $units) {
-        $retval = array();
-        foreach ($units['unitNumbers'] as $unit) {
-            $retval[$unit['id']] = $unit['unitNumber'];
-        }
-        return $retval;
     }
 
 }
