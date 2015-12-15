6d687c3780d2ce22c67a8288f55815f1c05d9aa4
Nathan DeVuono <nathan.devuono@gmail.com>
2015-12-15 22:30:20 +0000

    Fixing delete checking - would never be true before

--- src/extensions/angular/ng/js/rootApp.js
+++ src/extensions/angular/ng/js/rootApp.js
@@ -18,14 +18,14 @@ module.config(function ($httpProvider) {
     	return {
     		'response': function(response) {
     			if (response && 
-				response.config.method === 'POST' && 
-                response.config.method === 'DELETE' && 
+				(response.config.method === 'POST' || 
+                response.config.method === 'DELETE') && 
 				response.data.result === 'error') {
     				toastsSrv.newAlert(response.data);
                     validationSrv.showErrors(response.data);
     			} else if (response && 
-				response.config.method === 'POST' && 
-                response.config.method === 'DELETE' && 
+				(response.config.method === 'POST' || 
+                response.config.method === 'DELETE') && 
 				response.data.result !== 'error') {
     				toastsSrv.newAlert({'data': { 'Success' : {'Success' : 'Action Completed'} }, 'result': 'success'});
     			}
