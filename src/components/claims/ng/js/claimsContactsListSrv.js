bb9e4102fcb78e70a1088fe04ccec8ad0d1fb942
dcdrawk <dcdrawk@gmail.com>
2016-01-05 15:44:29 -0800

    cleaned up some code comments

--- src/components/claims/ng/js/claimsContactsListSrv.js
+++ src/components/claims/ng/js/claimsContactsListSrv.js
@@ -4,13 +4,11 @@ module.service('contactListSrv', function(searchSrv, crudSrv) {
             config[type] = value;
 
             return searchSrv.autocomplete('/admin/contacts/', config).then(function(response){
-                //console.log(response);
                 if(response.data.ContactsCount[0].rowCount > 0) { 
                     return response.data.Contacts;
                 } else {
                     return undefined;
-                }
-        	
+                }        	
             });
 	};
 
