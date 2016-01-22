72669b9f4a70691b693b8df539cf9287c6fc9bdb
dcdrawk <dcdrawk@gmail.com>
2016-01-22 09:02:46 -0800

    modified the button on the locations section to be a cog

--- src/components/claims/views/admin/claims/edit.php
+++ src/components/claims/views/admin/claims/edit.php
@@ -40,9 +40,12 @@
         </div>
         <div class="col-xs-12 col-md-6 no-padding">
             <div class="card" ng-model="claim">
-                <div class="cardheader row">
-                    <h1 class="col-xs-9"><?php echo $this->getString('CLAIMS_SUMMARY'); ?></h1>
-                    <div class="col-xs-3 text-right row-controls">
+                <div class="cardheader">
+                    <div class="pull-left">
+                        <h1><?php echo $this->getString('CLAIMS_SUMMARY'); ?></h1>
+                    </div>
+
+                    <div class="pull-right text-right row-controls">
                         <div class="dropdown">
                             <button class="btn btn-default dropdown-toggle glyphicon glyphicon-cog" type="button"
                                     id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
@@ -78,33 +81,48 @@
         </div>
 
         <div class="col-xs-12 no-padding">
-            <div class="widget" ng-controller="claimsLocationsListCtrl">
-                <div class="widgetheader">
-                    <h2 class="pull-left"><?php echo $this->getString('CLAIMS_LOCATIONS') ?></h2>
-                    <div class="pull-right">
-                        <button class="primary" ng-click="openClaimLocationModal()">
-                            <?php echo $this->getString('CLAIMS_LOCATIONS_ADDNEW') ?>
-                        </button>
+            <div class="card" ng-controller="claimsLocationsListCtrl">
+                <div class="cardheader">
+                    <h1 class="pull-left"><?php echo $this->getString('CLAIMS_LOCATIONS') ?></h1>
+                    <!--                    <div class="pull-right">
+                                            <button class="primary" ng-click="openClaimLocationModal()">
+                    <?php // echo $this->getString('CLAIMS_LOCATIONS_ADDNEW') ?>
+                                            </button>
+                                        </div>-->
+
+                    <div class="pull-right text-right row-controls">
+                        <div class="dropdown">
+                            <button class="btn btn-default dropdown-toggle glyphicon glyphicon-cog" type="button"
+                                    id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            </button>
+                            <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
+                                <li>
+                                    <a href="#" ng-click="openClaimLocationModal()">
+                                        <?php echo $this->getString('CLAIMS_LOCATIONS_ADDNEW') ?>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
                 <ul class="table table-striped table-hover flex-table">
                     <li class="head">
-                        <div><?php echo $this->getString('CLAIMS_JOBNUMBER'); ?></div>
-                        <div><?php echo $this->getString('CLAIMS_PHASE'); ?></div>
-                        <div><?php echo $this->getString('CLAIMS_BUZZER'); ?></div>
-                        <div><?php echo $this->getString('CLAIMS_PARENT_CLAIM'); ?></div>
+                        <div column-sortable data-column="jobNumber"><?php echo $this->getString('CLAIMS_JOBNUMBER'); ?></div>
+                        <div column-sortable data-column="phase"><?php echo $this->getString('CLAIMS_PHASE'); ?></div>
+                        <div column-sortable data-column="buzzerCode"><?php echo $this->getString('CLAIMS_BUZZER'); ?></div>
+                        <div column-sortable data-column="parentClaim"><?php echo $this->getString('CLAIMS_PARENT_CLAIM'); ?></div>
                         <div sort-by-button class="cog-col row-controls">&nbsp;</div>
                     </li>
                     <div class="flex-tbody">
-                        <li ng-if="loading || !loading" class="flex-loading">
+                        <li ng-if="loading" class="flex-loading">
                             <div></div>
                             <div></div>
                             <div><span class="spinner-loader"></span></div>
                             <div></div>
                             <div></div>
                         </li>
-                        <li ng-repeat="location in claimsLocations" class="flex-row"  ng-class="getStatusColor(location)">
+                        <li ng-repeat="location in claimsLocations" class="flex-row"  ng-class="getStatusColor(location)" ng-click="selectRow(location)">
                             <div class="flex-left">
                                 <div>
                                     <div class="content"><h4><?php echo $this->getString('CLAIMS_UNIT_NUMBER') ?>{{location.unitNumber}}</h4></div>
@@ -179,16 +197,16 @@
                     <thead>
                         <tr>
                             <th column-sortable data-column="jobNumber">
-                                <?php // echo $this->getString('CLAIMS_JOBNUMBER'); ?>
+                <?php // echo $this->getString('CLAIMS_JOBNUMBER'); ?>
                             </th>
                             <th column-sortable data-column="phase">
-                                <?php // echo $this->getString('CLAIMS_PHASE'); ?>
+                <?php // echo $this->getString('CLAIMS_PHASE'); ?>
                             </th>
                             <th column-sortable data-column="buzzerCode">
-                                <?php // echo $this->getString('CLAIMS_BUZZER'); ?>
+                <?php // echo $this->getString('CLAIMS_BUZZER'); ?>
                             </th>
                             <th column-sortable data-column="parentClaim">
-                                <?php // echo $this->getString('CLAIMS_PARENT_CLAIM'); ?>
+                <?php // echo $this->getString('CLAIMS_PARENT_CLAIM'); ?>
                             </th>
                             <th sort-by-button class="cog-col row-controls">&nbsp;</th>
                         </tr>
@@ -216,13 +234,13 @@
                                             aria-haspopup="true" aria-expanded="true">
                                     </button>
                                     <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
-                                        <li><a href="" ng-click="openClaimLocationModal(location)"><?php // echo $this->getString('CLAIMS_EDIT_LOCATION') ?></a></li>
-                                        <li><a href="" ng-click="openCustomersModal('customersModal', location, {})"><?php // echo $this->getString('CLAIMS_ADD_CUSTOMER') ?></a></li>
-                                        <li><a href="/admin/claim/initial-jobsheet/edit/{{location.Claims_id}}/{{location.id}}"><?php // echo $this->getString('CLAIMS_EDIT_INITIAL_JOBSHEET') ?></a></li>
-                                        <li><a href="/admin/claim/initial-jobsheet/view/{{location.Claims_id}}/{{location.id}}"><?php // echo $this->getString('CLAIMS_VIEW_INITIAL_JOBSHEET') ?></a></li>
-                                        <li><a href="/admin/scoping/takeoffs/{{location.Claims_id}}/{{location.id}}"><?php // echo $this->getString('CLAIMS_SCOPING_MATERIAL_TAKEOFFS') ?></a></li>
+                                        <li><a href="" ng-click="openClaimLocationModal(location)"><?php // echo $this->getString('CLAIMS_EDIT_LOCATION')               ?></a></li>
+                                        <li><a href="" ng-click="openCustomersModal('customersModal', location, {})"><?php // echo $this->getString('CLAIMS_ADD_CUSTOMER')               ?></a></li>
+                                        <li><a href="/admin/claim/initial-jobsheet/edit/{{location.Claims_id}}/{{location.id}}"><?php // echo $this->getString('CLAIMS_EDIT_INITIAL_JOBSHEET')               ?></a></li>
+                                        <li><a href="/admin/claim/initial-jobsheet/view/{{location.Claims_id}}/{{location.id}}"><?php // echo $this->getString('CLAIMS_VIEW_INITIAL_JOBSHEET')               ?></a></li>
+                                        <li><a href="/admin/scoping/takeoffs/{{location.Claims_id}}/{{location.id}}"><?php // echo $this->getString('CLAIMS_SCOPING_MATERIAL_TAKEOFFS')               ?></a></li>
                                         <li>
-                                            <a gcms="{uri='admin_claims_secondarysheets_home' params='{{location.Claims_id}}/{{location.id}}'}"><?php // echo $this->getString('CLAIMS_SECONDARY_SHEETS'); ?></a>
+                                            <a gcms="{uri='admin_claims_secondarysheets_home' params='{{location.Claims_id}}/{{location.id}}'}"><?php // echo $this->getString('CLAIMS_SECONDARY_SHEETS');               ?></a>
                                         </li>
                                         <li><a href="" ng-click="delete(location)"><?php echo $this->getString('REMOVE') ?></a></li>
                                     </ul>
