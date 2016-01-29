592eb3005acbeb42dd306c5be739da9f959a2f04
dcdrawk <dcdrawk@gmail.com>
2016-01-29 12:03:45 -0800

    added action attribute to form submit

--- src/components/staff/views/login/reset.php
+++ src/components/staff/views/login/reset.php
@@ -1,10 +1,8 @@
-
-
 <div class="center-block padding-vertical login-form">
     <img class="login-logo" src="/images/logos/logo.png">
     <div class="card login-card">
         <h1><?php echo $this->getString('LOGIN_RESET_FORM'); ?></h1>
-        <form role="form" method="post">
+        <form role="form" action method="post">
             <div class="form-group">
                 <label for="email"><?php echo $this->getString('LOGIN_ENTER_USERNAME'); ?></label>
                 <?php echo $form['username']; ?>
