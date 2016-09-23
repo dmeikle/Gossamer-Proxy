4d5ac9133dd34a8950058c1da726d14e736ce1f6
Dave Meikle <david@quantumunit.com>
2016-09-23 14:21:21 -0700

    refreshed gitignore

--- app/framework/core/components/widgets/views/super/context-button.php
+++
@@ -1,5 +0,0 @@
-<div class="btn-group" ng-controller="widgetsCtrl">
-    <button type="button" class="primary" ng-click="addNewWidget()">
-        <?php echo $this->getString('WIDGET_NEW'); ?>
-    </button>
-</div>
